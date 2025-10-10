15e212bb6ce1d313cf8cfa5ab8305ceb44635d3c
Quentin Macheda <contact@qmweb.fr>
2025-10-10 21:44:28 +0800

    chore(global): initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Test technique | BeeAgency",
+  description: "Test technique | BeeAgency",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fr">
+      <body className={`${inter.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
