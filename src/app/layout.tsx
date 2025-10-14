cb15ffa9696485bcc18a4edeb071a5842025c274
Quentin Macheda <contact@qmweb.fr>
2025-10-14 22:28:59 +0800

    chore(global): update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,25 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
 
 const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-});
+	variable: "--font-inter",
+	subsets: ["latin"],
+})
 
 export const metadata: Metadata = {
-  title: "Test technique | BeeAgency",
-  description: "Test technique | BeeAgency",
-};
+	title: "Launch UI | BeeAgency",
+	description: "Launch UI | BeeAgency",
+}
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <html lang="fr">
-      <body className={`${inter.variable} antialiased`}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="fr">
+			<body className={`${inter.variable} antialiased`}>{children}</body>
+		</html>
+	)
 }
